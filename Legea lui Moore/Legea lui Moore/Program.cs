061a3009704a2714c764321061046f2d98f65f00
Bianca <biie.chiruc@yahoo.com>
2020-10-17 23:05:53 +0300

    Legea lui Moore ( Chiruc Bianca)

---
+++ Legea lui Moore/Legea lui Moore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legea_lui_Moore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double m=0;
+            int n, t=0;
+            n = int.Parse(Console.ReadLine());
+            while(m<n)
+            {
+                m = m + 0.11;
+                t++;
+            }
+            Console.WriteLine(t-1);
+            Console.ReadKey();
+        }
+    }
+}
